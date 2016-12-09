1e0274d34e41b88ee58e08c0ce1144dd7bcc4f50
arichika taniguchi <email@arichika.net>
2016-12-09 18:21:07 +0900

    add  GetFirstImageUriAsync extention

--- src/MediumProxy.Test/ForDebugRun.cs
+++ src/MediumProxy.Test/ForDebugRun.cs
@@ -1,9 +1,11 @@
-﻿using System.Diagnostics;
+﻿using System;
+using System.Diagnostics;
 using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.Extensions.Logging;
 using MediumProxy;
+using MediumProxy.Model;
 using Xunit;
 
 namespace MediumProxy.Test
@@ -19,6 +21,29 @@ namespace MediumProxy.Test
                 Assert.True(content.Any());
                 Debug.WriteLine(content.First());
             }
+        }
+
+        [Fact]
+        public async Task GetFirstImageUriAsync()
+        {
+            var item = new Item("dummy")
+            {
+                Description =
+                    "<div class=\"medium-feed-item\"><p class=\"medium-feed-image\"><a href=\"https://medium.com/@Medium/personalize-your-medium-experience-with-users-publications-tags-26a41ab1ee0c?source=rss-504c7870fdb6------2\"><img src=\"https://cdn-images-1.medium.com/max/1024/1*Y_AoLGDT2ku9uuksLQDk4w.png\" width=\"1024\"></a></p><p class=\"medium-feed-snippet\">Following users, publications, and tags on Medium</p><p class=\"medium-feed-link\"><img src=\"https://cdn-images-1.medium.com/max/1024/1*Y_AoLGDT2ku9uuksLQDk4w.png\" width=\"1024\">",
+            };
+            Assert.True(item.Description.Contains((await item.GetFirstImageUriAsync()).AbsoluteUri));
+
+            item.Description = @"dummy";
+            Assert.Null(await item.GetFirstImageUriAsync());
+
+            item.Description = @"dummy";
+            Assert.True(new Uri("about:blank").AbsoluteUri == (await item.GetFirstImageUriAsync(new Uri("about:blank"))).AbsoluteUri);
+
+            item = null;
+            Assert.Null(await item.GetFirstImageUriAsync());
+
+            item = new Item("dummy");
+            Assert.Null(await item.GetFirstImageUriAsync());
         }
 
         [Fact]
---
+++ src/MediumProxy/MediumProxyExtentions.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using MediumProxy.Model;
+
+namespace MediumProxy
+{
+    public static class MediumProxyExtentions
+    {
+        private static readonly Regex RegexFirstImageUrl = new Regex("<img[^<]+src=\"([^<\"]+)\"[^<]+>");
+
+        public static async Task<Uri> GetFirstImageUriAsync(this Item item, Uri defaultUrl = null)
+        {
+            if (string.IsNullOrEmpty(item?.Description)) return defaultUrl;
+
+            var match = RegexFirstImageUrl.Match(item.Description);
+
+            if (match.Groups.Count < 1) return defaultUrl;
+
+            try
+            {
+                return new Uri(match.Groups[1].Value);
+            }
+            catch (Exception ex)
+            {
+                return defaultUrl;
+            }
+        }
+    }
+}
